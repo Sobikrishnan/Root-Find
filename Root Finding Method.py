a17a3eb209924a6386ffadd3a5e06adea385e86b
R K Sobithan <sobikrishnan001@gmail.com>
2025-03-31 15:16:23 +0530

    Add files via upload

---
+++ Root Finding Method.py
@@ -0,0 +1,12 @@
+#x=[-b±√(b²-4ac)]/2a
+#x²+9x+14=0
+#x**2+9*x+14=0
+
+a=1
+b=9
+c=14
+delta = (b**2-4*a*c)**0.5
+
+x1 = (-b+delta)/2*a
+x2 = (-b-delta)/2*a
+print("The roots of the x1:",x1,"and x2:",x2)
